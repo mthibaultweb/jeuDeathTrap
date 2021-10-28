052c8c32a859f3c75c667757259091cde86cbfb1
mthibault006 <thibault.maxime@outlook.com>
2021-10-28 12:57:22 +0200

    Assets background platforms and decorations

--- death_trap/Assets/Scripts/CharacterBehavior.cs
+++ death_trap/Assets/Scripts/CharacterBehavior.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class CharacterBehavior : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private SpriteRenderer mainSprite;
+    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private float MoveSpeed;
+    [SerializeField] private float JumpHeight;
+    [SerializeField] private float groundDistance;
+    [SerializeField] LayerMask groundMask;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,37 @@ public class CharacterBehavior : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Move()
+    {
+        float currentMoveSpeed = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentMoveSpeed = -MoveSpeed;
+            mainSprite.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentMoveSpeed = MoveSpeed;
+            mainSprite.flipX = false;
+        }
+
+        rb2D.velocity = new Vector2(currentMoveSpeed, rb2D.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(currentMoveSpeed));
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb2D.position, Vector2.down, groundDistance, groundMask);
+        Debug.DrawRay(rb2D.position, Vector2.down * groundDistance, Color.red);
+        return hit.collider != null; 
+        // return false;
+    }
+
+    public void Jump()
+    {
+        rb2D.velocity = new Vector2(rb2D.velocity.x, JumpHeight);
     }
 }
--- death_trap/Assets/Scripts/PlayerControls.cs
+++ death_trap/Assets/Scripts/PlayerControls.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour
+public class PlayerControls : CharacterBehavior
 {
-    [SerializeField] private Rigidbody2D rb2D;
-    [SerializeField] private SpriteRenderer mainSprite;
-    [SerializeField] private PolygonCollider2D playerCollider;
-    [SerializeField] private float MoveSpeed = 100f;
-    [SerializeField] private float JumpHeight = 200f;
-    [SerializeField] private float groundDistance = 10f;
-    [SerializeField] LayerMask groundMask;
-    public Animator animator;
+    // [SerializeField] private Rigidbody2D rb2D;
+    // [SerializeField] private SpriteRenderer mainSprite;
+    // [SerializeField] private PolygonCollider2D playerCollider;
+    // [SerializeField] private float MoveSpeed = 100f;
+    // [SerializeField] private float JumpHeight = 200f;
+    //[SerializeField] private float groundDistance = 10f;
+    // [SerializeField] LayerMask groundMask;
+    // public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -24,57 +24,41 @@ public class PlayerControls : MonoBehaviour
     {
         Move();
 
+        
         if(IsGrounded())
         {
-            Jump();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
         }
-        
     }
 
-    public void Move()
-    {
-        float currentMoveSpeed = 0f;
-   /*     float moveLeft = Input.GetAxis("Horizontal");
-        Debug.Log(moveLeft);*/
+    // public void Move()
+    // {
+    //     float currentMoveSpeed = 0f;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            currentMoveSpeed = -MoveSpeed;
-            mainSprite.flipX = true;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            currentMoveSpeed = MoveSpeed;
-            mainSprite.flipX = false;
-        }
+    //     if (Input.GetKey(KeyCode.LeftArrow))
+    //     {
+    //         currentMoveSpeed = -MoveSpeed;
+    //         mainSprite.flipX = true;
+    //     }
+    //     else if (Input.GetKey(KeyCode.RightArrow))
+    //     {
+    //         currentMoveSpeed = MoveSpeed;
+    //         mainSprite.flipX = false;
+    //     }
 
-        rb2D.velocity = new Vector2(currentMoveSpeed, rb2D.velocity.y);
-        animator.SetFloat("Speed", Mathf.Abs(currentMoveSpeed));
-    }
+    //     rb2D.velocity = new Vector2(currentMoveSpeed, rb2D.velocity.y);
+    //     animator.SetFloat("Speed", Mathf.Abs(currentMoveSpeed));
+    // }
 
-    public bool IsGrounded()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(rb2D.position, Vector2.down, groundDistance, groundMask);
-        Debug.DrawRay(rb2D.position, Vector2.down * groundDistance, Color.red);
-        return hit.collider != null; 
+    // public bool IsGrounded()
+    // {
+    //     RaycastHit2D hit = Physics2D.Raycast(rb2D.position, Vector2.down, groundDistance, groundMask);
+    //     Debug.DrawRay(rb2D.position, Vector2.down * groundDistance, Color.red);
+    //     return hit.collider != null; 
 
-    }
-
-    public void Jump()
-    {
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, JumpHeight);
-        }
-
-        /* if (Input.GetAxis("Jump"))
-         {
-             rb2D.velocity = new Vector2(rb2D.velocity.x, JumpHeight);
-         }*/
-
-
-
-    }
+    // }
 }
 
